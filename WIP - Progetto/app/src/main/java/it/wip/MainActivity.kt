8dc6537ce13ac581686dd337579db5d4c8193cb0
emanuelefrisi <s1092866@studenti.univpm.it>
2022-05-06 16:04:43 +0200

    Eliminato tutto ciò che era relativo al fragment 'menu'

--- WIP - Progetto/app/src/main/java/it/wip/MainActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/MainActivity.kt
@@ -2,9 +2,7 @@ package it.wip
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import it.wip.ui.HeaderFragment
-import it.wip.ui.MenuFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +12,6 @@ class MainActivity : AppCompatActivity() {
         val transaction = supportFragmentManager.beginTransaction()
 
         transaction.add(R.id.main_layout, HeaderFragment())
-
-        transaction.add(R.id.main_layout, MenuFragment())
 
         transaction.commit()
 
--- WIP - Progetto/app/src/main/java/it/wip/ui/HeaderFragment.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/HeaderFragment.kt
@@ -1,9 +1,5 @@
 package it.wip.ui
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import it.wip.R
 
