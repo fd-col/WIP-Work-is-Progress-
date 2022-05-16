18bbcba1998e58a4e5a0cd34b155af06648e4816
KevinGiusti <S1092345@studenti.univpm.it>
2022-05-16 23:28:38 +0200

    inizio xml start_story
    inizio xml start_story

--- WIP - Progetto/app/src/main/java/it/wip/MainActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/MainActivity.kt
@@ -1,26 +1,42 @@
 package it.wip
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Chronometer
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.res.ResourcesCompat
 import it.wip.ui.FrameFragment
 import it.wip.ui.HeaderFragment
 import it.wip.ui.MenuFragment
+import it.wip.ui.StartStoryActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val transaction = supportFragmentManager.beginTransaction()
+        setContentView(R.layout.activity_start_story)
+        val cronometro = findViewById<Chronometer>(R.id.simpleChronometer)
+        cronometro.start()
+        cronometro.setTypeface(ResourcesCompat.getFont(this, R.font.press_start_2p))
 
-        transaction.add(R.id.header_layout, HeaderFragment())
+        //val slider = findViewById<com.google.android.material.slider.Slider>(R.id.seekBar_story_time)
+        //slider.setLabelFormatter { value: Float ->
+        //    "${value.toInt()}min/${60-value.toInt()}min"
+        //}
 
-        transaction.add(R.id.frame_layout, FrameFragment())
 
-        transaction.add(R.id.menu_layout, MenuFragment())
+        //setContentView(R.layout.activity_main)
 
-        transaction.commit()
+        //val transaction = supportFragmentManager.beginTransaction()
 
+        //transaction.add(R.id.header_layout, HeaderFragment())
+
+        //transaction.add(R.id.frame_layout, FrameFragment())
+
+        //transaction.add(R.id.menu_layout, MenuFragment())
+
+        //transaction.commit()
     }
 }
--- WIP - Progetto/app/src/main/java/it/wip/ui/FrameFragment.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/FrameFragment.kt
@@ -1,5 +1,6 @@
 package it.wip.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
@@ -7,9 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import it.wip.MainActivity
 import it.wip.R
 
-class FrameFragment : Fragment(R.layout.fragment_frame) {
+class FrameFragment: Fragment(R.layout.fragment_frame) {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,7 +19,6 @@ class FrameFragment : Fragment(R.layout.fragment_frame) {
         savedInstanceState: Bundle?
     ): View? {
         val view = super.onCreateView(inflater, container, savedInstanceState)
-
         val playButton = view?.findViewById<ImageButton>(R.id.play_button)
 
         playButton?.setOnClickListener {
@@ -29,10 +30,11 @@ class FrameFragment : Fragment(R.layout.fragment_frame) {
                 MotionEvent.ACTION_DOWN -> playButton.setImageResource(R.drawable.play_button_pressed)
                 MotionEvent.ACTION_UP -> playButton.setImageResource(R.drawable.play_button)
             }
-
             v?.onTouchEvent(event) ?: true
+
         }
 
         return view
     }
+
 }
