b5954e7dfdb012e92e877326bc24cf88037a7eb6
fd-col <s1093242@studenti.univpm.it>
2022-05-22 17:38:06 +0200

    add empty xml and class for kindom-section

---
+++ WIP - Progetto/app/src/main/java/it/wip/ui/KingdomActivity.kt
@@ -0,0 +1,12 @@
+package it.wip.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import it.wip.R
+
+class KingdomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kingdom)
+    }
+}
