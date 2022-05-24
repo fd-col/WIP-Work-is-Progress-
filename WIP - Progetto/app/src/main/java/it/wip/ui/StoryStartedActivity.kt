045252553b0e4899485c3228e801e6e9c757e024
KevinGiusti <S1092345@studenti.univpm.it>
2022-05-25 00:53:19 +0200

    startStoryActivity
    startStoryActivity

--- WIP - Progetto/app/src/main/java/it/wip/ui/StoryStartedActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/StoryStartedActivity.kt
@@ -1,5 +1,20 @@
 package it.wip.ui
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Chronometer
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import it.wip.R
 
-class StoryStartedActivity : AppCompatActivity()
+class StoryStartedActivity : AppCompatActivity(){
+    @SuppressLint("ClickableViewAccessibility", "UseSwitchCompatOrMaterialCode")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_story_started)
+
+        val cronometro = findViewById<Chronometer>(R.id.simpleChronometer)
+        cronometro.start()
+        cronometro.setTypeface(ResourcesCompat.getFont(this, R.font.press_start_2p))
+    }
+}
