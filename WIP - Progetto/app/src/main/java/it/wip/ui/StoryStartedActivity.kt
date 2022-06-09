3c4e0f6cec11861b68eae16d06bab286f7080938
KevinGiusti <S1092345@studenti.univpm.it>
2022-06-09 17:20:49 +0200

    algoritmo scelta ed evoluzione background
    algoritmo scelta ed evoluzione background

--- WIP - Progetto/app/src/main/java/it/wip/ui/StoryStartedActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/StoryStartedActivity.kt
@@ -8,7 +8,6 @@ import android.view.MotionEvent
 import android.widget.Chronometer
 import android.widget.ImageButton
 import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import it.wip.MainActivity
@@ -22,30 +21,81 @@ class StoryStartedActivity : AppCompatActivity(){
         setContentView(R.layout.activity_story_started)
 
         /*
-        val progresso = findViewById<ProgressBar>(R.id.progressBar)
-        progresso.max = 100
-        progresso.progress = 50
-        */
+        * in riferimento al listener "setOnChronometerTickListener", che esegue il contenuto del
+        * blocco {} ogni volta che trascorre un secondo, le variabili first, second e
+        * thirdAlreadyExecuted garantiscono che la funzione "backgroundEvolution" venga eseguita
+        * una sola volta per slot temporale
+        * */
+        var firstAlreadyExecuted = true
+        var secondAlreadyExecuted = true
+        var thirdAlreadyExecuted = true
+
+        //slot temporali di prova; sostituire con quelli ricavati dallo slider
+        var maxTime = 40000
+        var timeRange1 = (5000..14000)
+        var timeRange2 = (15000..20000)
+        var timeRange3 = (21000..30000)
+
+
         val stopButton = findViewById<ImageButton>(R.id.stop_button)
         val artwork = findViewById<ImageView>(R.id.canva)
         val cronometro = findViewById<Chronometer>(R.id.simpleChronometer)
-        cronometro.setOnChronometerTickListener {
-            var timeElapsed = (SystemClock.elapsedRealtime() - cronometro.base).toInt()
-            if(timeElapsed>3000 && timeElapsed<4000){
-                Toast.makeText(this, "ciao", Toast.LENGTH_SHORT).show()
-                artwork.setBackgroundResource(R.drawable.hopper_1)
-            }
-
-            //Toast.makeText(this, "ciao", Toast.LENGTH_SHORT).show()
-        }
-
-
-        //backgroundSelector(artwork, cronometro)
-
-
-
         cronometro.start()
         cronometro.typeface = ResourcesCompat.getFont(this, R.font.press_start_2p)
+
+        //invocazione metodo per scelta casuale del primo background tra quelli disponibili
+        var selectedArtwork = backgroundSelector(artwork)
+
+        //listener che gestisce cosa fare a schermo ogni volta che il tempo incrementa
+        cronometro.setOnChronometerTickListener {
+            val myTime = (SystemClock.elapsedRealtime() - cronometro.base).toInt()
+
+            /*
+            * if(myTime>=maxTime){...} è il blocco di codice che mette un nuovo quadro e lo fa
+            * evolvere quando un quadro viene completamente dipinto
+            * */
+            if(myTime>=maxTime){
+                firstAlreadyExecuted = true
+                secondAlreadyExecuted = true
+                thirdAlreadyExecuted = true
+                timeRange1 = (maxTime+5000..maxTime+14000)
+                timeRange2 = (maxTime+15000..maxTime+20000)
+                timeRange3 = (maxTime+21000..maxTime+30000)
+                maxTime+=maxTime
+
+                // porzione di codice che evita di farmi vedere due volte di fila lo stesso quadro
+                var newSelectedArtwork = backgroundSelector(artwork)
+                while(selectedArtwork == newSelectedArtwork){
+                    newSelectedArtwork = backgroundSelector(artwork)
+                }
+                selectedArtwork = newSelectedArtwork
+            }
+
+            /*
+            * when (myTime){...} è il blocco di codice che mi evolve il quadro a seconda dello slot
+            * temporale in cui mi trovo
+            * */
+            when (myTime){
+                in timeRange1 -> {
+                    if(firstAlreadyExecuted) {
+                        backgroundEvolution(artwork, selectedArtwork, 2)
+                        firstAlreadyExecuted = false
+                    }
+                }
+                in timeRange2 -> {
+                    if(secondAlreadyExecuted) {
+                        backgroundEvolution(artwork, selectedArtwork, 3)
+                        secondAlreadyExecuted = false
+                    }
+                }
+                in timeRange3 -> {
+                    if(thirdAlreadyExecuted) {
+                        backgroundEvolution(artwork, selectedArtwork, 4)
+                        thirdAlreadyExecuted = false
+                    }
+                }
+            }
+        }
 
         stopButton?.setOnTouchListener { v, event ->
             when (event?.action) {
@@ -56,62 +106,64 @@ class StoryStartedActivity : AppCompatActivity(){
         }
 
         stopButton?.setOnClickListener {
+            //firstAlreadyExecuted = true
+            //secondAlreadyExecuted = true
+            //thirdAlreadyExecuted = true
             cronometro.stop()
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
-}
 
-//          scelta dinamica del quadro
-fun backgroundSelector(artwork: ImageView, tempo: Chronometer){
-    when ((1..3).random()) {
-        1 -> {
-            artwork.setBackgroundResource(R.drawable.dali_1)
-            tempo.onChronometerTickListener = Chronometer.OnChronometerTickListener {
-                backgroundEvolution(tempo, 1, artwork)
-            }
-        }
-        2 -> {
-            artwork.setBackgroundResource(R.drawable.hopper_1)
-            backgroundEvolution(tempo, 2, artwork)
-        }
-        3 -> {
-            artwork.setBackgroundResource(R.drawable.munch_bg_1)
-            backgroundEvolution(tempo, 3, artwork)
-        }
-    }
-}
 
-fun backgroundEvolution(tempo: Chronometer, artworkId: Int, artwork: ImageView){
-    val elapsed = (SystemClock.elapsedRealtime() - tempo.base).toInt()
-    if(elapsed>5) {
-        when (artworkId) {
+
+
+    //          SCELTA DINAMICA DEL QUADRO
+    fun backgroundSelector (artwork: ImageView): Int{
+        var selectedArtwork = 1
+
+        //il range è 1..3 perchè all'inizio abbiamo solo 3 quadri sbloccati
+        when ((1..3).random()) {
             1 -> {
-                artwork.setBackgroundResource(R.drawable.dali_2)
+                artwork.setBackgroundResource(R.drawable.dali_1)
+                selectedArtwork = 1
             }
             2 -> {
-                artwork.setBackgroundResource(R.drawable.hopper_2)
+                artwork.setBackgroundResource(R.drawable.hopper_1)
+                selectedArtwork = 2
             }
             3 -> {
-                artwork.setBackgroundResource(R.drawable.munch_bg_2)
+                artwork.setBackgroundResource(R.drawable.munch_bg_1)
+                selectedArtwork = 3
+            }
+        }
+        return selectedArtwork
+    }
+
+    //          AUTO-AGGIORNAMENTO DIPINTO MENTRE SCORRE IL TIMER
+    fun backgroundEvolution(artwork: ImageView, artworkId: Int, artworkCurrentState:Int){
+
+        when (artworkId) {
+            1 -> {
+                when (artworkCurrentState) {
+                    2 -> artwork.setBackgroundResource(R.drawable.dali_2)
+                    3 -> artwork.setBackgroundResource(R.drawable.dali_3)
+                    4 -> artwork.setBackgroundResource(R.drawable.dali_4)
+                }
+            }
+            2 -> {
+                when (artworkCurrentState) {
+                    2 -> artwork.setBackgroundResource(R.drawable.hopper_2)
+                    3 -> artwork.setBackgroundResource(R.drawable.hopper_3)
+                    4 -> artwork.setBackgroundResource(R.drawable.hopper_4)
+                }
+            }
+            3 -> {
+                when (artworkCurrentState) {
+                    2 -> artwork.setBackgroundResource(R.drawable.munch_bg_2)
+                    3 -> artwork.setBackgroundResource(R.drawable.munch_bg_3)
+                    4 -> artwork.setBackgroundResource(R.drawable.munch_bg_4)
+                }
             }
         }
     }
 }
-
-/*
-fun shotClockStart(v: View?) {
-    val shotclock = findViewById(R.id.chrono1) as Chronometer
-    shotclock.onChronometerTickListener =
-        OnChronometerTickListener { chronometer ->
-            val timeElapsed = SystemClock.elapsedRealtime() - chronometer.base
-            if (timeElapsed >= 30000) {
-                //  HERE I WANT A VIBRATION ON THE DEVICE.
-            } else if (timeElapsed >= 60000) {
-                //HERE I WANT A NOTIFICATION ALERT
-            }
-        }
-    shotclock.base = SystemClock.elapsedRealtime()
-    shotclock.start()
-}
-* */
