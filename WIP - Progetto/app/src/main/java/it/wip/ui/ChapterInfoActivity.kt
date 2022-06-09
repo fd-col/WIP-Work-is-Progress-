e02e0f4f9f692a5cd2015954ac99019458ead7cb
fd-col <s1093242@studenti.univpm.it>
2022-06-09 13:54:50 +0200

    adjust chapterInfo.xml & make Settings work properly

--- WIP - Progetto/app/src/main/java/it/wip/ui/ChapterInfoActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/ChapterInfoActivity.kt
@@ -1,6 +1,9 @@
 package it.wip.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.ImageButton
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
@@ -11,9 +14,24 @@ class ChapterInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chapter_info)
 
+        val backButton = findViewById<ImageButton>(R.id.back_button_story_detail)
         val switchSilentMode = findViewById<Switch>(R.id.switch_silent_mode_chapter_info)
         val switchHardcoreMode = findViewById<Switch>(R.id.switch_hardcore_mode_chapter_info)
         switchSilentMode.setTypeface(ResourcesCompat.getFont(this, R.font.press_start_2p))
         switchHardcoreMode.setTypeface(ResourcesCompat.getFont(this, R.font.press_start_2p))
+
+
+
+        backButton.setOnClickListener {
+            startActivity(Intent(this, StoryDetailActivity::class.java))
+        }
+
+        backButton.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> backButton.setImageResource(R.drawable.back_arrow_pressed)
+                MotionEvent.ACTION_UP -> backButton.setImageResource(R.drawable.back_arrow)
+            }
+            v?.onTouchEvent(event) ?: true
+        }
     }
 }
--- WIP - Progetto/app/src/main/java/it/wip/ui/SettingsActivity.kt
+++ WIP - Progetto/app/src/main/java/it/wip/ui/SettingsActivity.kt
@@ -3,22 +3,24 @@ package it.wip.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import it.wip.R
+import it.wip.ui.fragments.HeaderFragment
+import it.wip.ui.fragments.MenuFragment
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-/*
-        val slider = findViewById<com.google.android.material.slider.Slider>(R.id.seekBar_story_time2)
+
+        val slider = findViewById<com.google.android.material.slider.Slider>(R.id.seekBar_settings)
         slider.setLabelFormatter { value: Float ->
             "${value.toInt()} min study/${60-value.toInt()} min pause"
         }
 
         // add the menu fragment to the bottom of KingdomActivity
         val transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.header_layout, HeaderFragment())
         transaction.add(R.id.menu_layout, MenuFragment())
         transaction.commit()
 
- */
     }
 }
